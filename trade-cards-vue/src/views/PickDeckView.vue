d0738e8d075a141ca633cffeacdade1ca7cb02f1
Lucas Garcia Ribeiro <lucasgr7@gmail.com>
2024-11-25 14:29:49 -0300

    PickDeck vue created

--- trade-cards-vue/src/views/PickDeckView.vue
+++ trade-cards-vue/src/views/PickDeckView.vue
@@ -6,6 +6,11 @@ const router = useRouter();
 function leave() {
   router.push('/');
 }
+
+function selectDeck(deckName: string) {
+  console.log(`O baralho escolhido foi: ${deckName}`);
+}
+
 </script>
 
 <template>
@@ -18,32 +23,36 @@ function leave() {
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
       </button>
-      <h1 class="text-3xl font-black text-outline-blue">Escolha seu tipo de baralho</h1>
+      <h1 class="text-3xl font-black text-outline-blue mb-8 ml-4">Escolha seu baralho</h1>
     </div>
     <div class="flex flex-col">
       <div class="px-16">
-        <div class="h-[300px] w-[210px]">
+        <div class="h-[300px] w-[210px] mb-2" @click="selectDeck('Baralho-Sol')">
           <img src="@/assets/baralho-sol.png" alt="Baralho-Sol">
         </div>
-        <div class="flex justify-between pt-1 pb-2">
-          <div>
-            <img src="@/assets/icons/energy.png" class="h-10 w-10" alt="Energia">5
+        <div class="flex justify-between pt-1 pb-8">
+          <div class="flex items-center border border-black rounded-full px-4 bg-yellow-300">
+            <img src="@/assets/icons/energy.png" class="h-10 w-10 pr-2 border-0 border-r-2 border-black" alt="Energia">
+            <p class="text-black ml-4">5</p>
           </div>
-          <div>
-            <img src="@/assets/icons/joker.png" class="h-10 w-10" alt="Coringas">2
+          <div class="flex items-center border border-black rounded-full px-4 bg-blue-300">
+            <img src="@/assets/icons/joker.png" class="h-10 w-10 pr-2 border-0 border-r-2 border-black" alt="Coringas">
+            <p class="text-black ml-4">2</p>
           </div>
         </div>
       </div>
-      <div class="border-0 border-t-2 p-16 border-black">
-        <div class="h-[300px] w-[210px]">
+      <div class="border-0 border-t-2 px-16 pt-12 border-black">
+        <div class="h-[300px] w-[210px] mb-2" @click="selectDeck('Baralho-Lua')">
           <img src="@/assets/baralho-lua.png" alt="Baralho-Lua">
         </div>
-        <div class="flex justify-between pt-1">
-          <div>
-            <img src="@/assets/icons/energy.png" class="h-10 w-10" alt="Energia">5
+        <div class="flex justify-between pt-2 pb-2">
+          <div class="flex items-center border border-black rounded-full px-4 bg-yellow-300">
+            <img src="@/assets/icons/energy.png" class="h-10 w-10 pr-2 border-0 border-r-2 border-black" alt="Energia">
+            <p class="text-black ml-4">3</p>
           </div>
-          <div>
-            <img src="@/assets/icons/joker.png" class="h-10 w-10" alt="Coringas">2
+          <div class="flex items-center border border-black rounded-full px-4 bg-blue-300">
+            <img src="@/assets/icons/joker.png" class="h-10 w-10 pr-2 border-0 border-r-2 border-black" alt="Coringas">
+            <p class="text-black ml-4">5</p>
           </div>
         </div>
       </div>
